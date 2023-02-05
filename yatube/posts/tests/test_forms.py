52652a24b229645ff74fae0c88cc440725b32158
Егор Вахитов <egor.vakhitov@yandex.ru>
2023-02-05 14:08:22 +0300

    Сортировка импортов.

--- yatube/posts/tests/test_forms.py
+++ yatube/posts/tests/test_forms.py
@@ -4,7 +4,6 @@ import tempfile
 from django.conf import settings
 from django.test import Client, TestCase, override_settings
 from django.urls import reverse
-
 from posts.models import Group, Post, User
 
 TEMP_MEDIA_ROOT = tempfile.mkdtemp(dir=settings.BASE_DIR)
